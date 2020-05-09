7db2c2808c91985271325f4b52b472bd4f525b07
danakotova <kotovadana7@gmail.com>
2020-05-09 22:42:41 +0300

    getting date from input that user choosed

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 import store from "../store/store";
 import "../plugins";
 import "./style.css";
+import formUI from "../views/forms";
 
 document.addEventListener("DOMContentLoaded", () => {
   initApp();
+
+  document.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(formUI.inputDateValue);
+
+  });
 
   async function initApp() {
     await store.init();
     store.getData();
   }
-
-  console.log(getDataPickerInstance(".datepicker"));
-  //example
 });
--- views/forms.js
+++ views/forms.js
@@ -1,3 +1,16 @@
-import {getDataPickerInstance} from '../plugins/materialize';
+import { getDataPickerInstance } from "../plugins/materialize";
 
-const 
+class FormUI {
+  constructor(dataPickerInstance) {
+    this.date = document.getElementById("datapicker");
+    this.inputDate = dataPickerInstance(this.date); // receive choosen data from input
+  }
+
+  get inputDateValue() {
+    return this.inputDate.toString(); // materialize - "gets a string representation of the selected date"
+  }
+}
+
+const formUI = new FormUI(getDataPickerInstance);
+
+export default formUI;
